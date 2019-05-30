70343f1a3d70e67b5a2cce57b373aea5716f5696
marta1511 <50339660+marta1511@users.noreply.github.com>
2019-05-30 09:44:17 +0200

    insert

--- imenik.sql
+++ imenik.sql
@@ -1,6 +1,13 @@
+#c:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < "D:\mysql\mysql\imenik.sql"
 drop database if exists imenik;
-create database imenik;
+create database imenik default character set utf8;
 use imenik;
+
+create table vrsta (
+sifra int not null primary key auto_increment,
+naziv varchar (50) not null
+);
+
 
 create table kontakt (
 sifra int not null primary key auto_increment,
@@ -10,10 +17,10 @@ broj varchar (20) not null,
 vrsta int
 );
 
-create table vrsta (
-sifra int not null primary key auto_increment,
-naziv varchar (50) not null
-);
 
 alter table kontakt add foreign key (vrsta) references vrsta (sifra);
+
+insert into vrsta (naziv) values ('mobitel'), ('kućni_telefon');
+insert into kontakt (ime,prezime,broj,vrsta) values ('leo','maras','0998752452',1), ('ante','anić','031583694',2), ('lana','barić','0915236478',1);
+
 
